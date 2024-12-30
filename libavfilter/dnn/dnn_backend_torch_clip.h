b84987b046d2a0acb36ef1c86fe254740cc98a7a
MaximilianKaindl <m.kaindl0208@gmail.com>
2024-12-30 23:26:19 +0100

    libavfilter/dnn: remove unnecessary exported methods and add logit_scale variable to THClipContext

--- libavfilter/dnn/dnn_backend_torch_clip.h
+++ libavfilter/dnn/dnn_backend_torch_clip.h
@@ -13,15 +13,12 @@ typedef struct THClipContext {
     std::unique_ptr<Tokenizer> tokenizer;
     std::vector<std::string> labels;
     std::string tokenizer_path;
+    float logit_scale;
 } THClipContext;
 
-torch::Tensor process_clip_similarity(const torch::Tensor& image_features, const torch::Tensor& text_embedding,DnnContext *ctx, float temperature);
-int create_tokenizer(THModel *th_model, std::string tokenizer_path);
 int init_clip_model(THModel *th_model, AVFilterContext *filter_ctx);
 void free_clip_context(THClipContext *clip_ctx);
 int fill_model_input_clip(THModel *th_model, THRequestItem *request, DNNData input);
-int encode_image_clip(THModel *th_model, THRequestItem *request);
-int encode_text_clip(THModel *th_model, THRequestItem *request);
 int set_params_clip(THModel *th_model, const char **labels, int label_count, const char *tokenizer_path);
 int process_clip_inference(THModel *th_model, THInferRequest *infer_request, const c10::Device& device, DnnContext *ctx);
 
