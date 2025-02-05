395564b640fd48e734a2c2a8dddace23f19d0d6c
MaximilianKaindl <m.kaindl0208@gmail.com>
2025-02-05 10:55:38 +0100

    libavfilter: use activate instead of filter_frame in avgclass and save output to file

--- libavfilter/vf_avgclass.c
+++ libavfilter/vf_avgclass.c
@@ -41,12 +41,15 @@ typedef struct AvgClassContext {
     const AVClass *clazz;
     int nb_classes;
     ClassProb *class_probs;
+    char *output_file;
 } AvgClassContext;
 
 #define OFFSET(x) offsetof(AvgClassContext, x)
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM | AV_OPT_FLAG_FILTERING_PARAM
 
 static const AVOption avgclass_options[] = {
+    { "output_file", "path to output file for averages",
+        OFFSET(output_file), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS },
     { NULL }
 };
 
@@ -60,17 +63,37 @@ static av_cold int init(AVFilterContext *ctx)
     return 0;
 }
 
-static av_cold void uninit(AVFilterContext *ctx)
+static void write_averages_to_file(AVFilterContext *ctx)
 {
     AvgClassContext *s = ctx->priv;
+    FILE *f;
+
+    if (!s->output_file) {
+        av_log(ctx, AV_LOG_WARNING, "No output file specified, skipping average output\n");
+        return;
+    }
+
+    f = avpriv_fopen_utf8(s->output_file, "w");
+    if (!f) {
+        av_log(ctx, AV_LOG_ERROR, "Could not open output file %s\n", s->output_file);
+        return;
+    }
 
     for (int i = 0; i < s->nb_classes; i++) {
         double avg = s->class_probs[i].count > 0 ?
                     s->class_probs[i].sum / s->class_probs[i].count : 0.0;
-
-        av_log(ctx, AV_LOG_INFO, "Classification Label:  %s: Average probability %.4f\n", s->class_probs[i].label, avg);
+        fprintf(f, "%s: %.4f\n", s->class_probs[i].label, avg);
+        av_log(ctx, AV_LOG_INFO, "Classification Label: %s: Average probability %.4f\n", 
+               s->class_probs[i].label, avg);
     }
 
+    fclose(f);
+}
+
+static av_cold void uninit(AVFilterContext *ctx)
+{
+    AvgClassContext *s = ctx->priv;
+    write_averages_to_file(ctx);
     av_freep(&s->class_probs);
 }
 
@@ -96,9 +119,8 @@ static ClassProb *find_or_create_class(AvgClassContext *s, const char *label)
     return &s->class_probs[s->nb_classes++];
 }
 
-static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
+static int process_frame(AVFilterContext *ctx, AVFrame *frame)
 {
-    AVFilterContext *ctx = inlink->dst;
     AvgClassContext *s = ctx->priv;
     AVFrameSideData *sd;
     const AVDetectionBBoxHeader *header;
@@ -117,7 +139,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
                 class_prob = find_or_create_class(s, bbox->classify_labels[j]);
                 if (!class_prob) {
-                    av_frame_free(&frame);
                     return AVERROR(ENOMEM);
                 }
 
@@ -126,23 +147,44 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
             }
         }
     }
-    return ff_filter_frame(ctx->outputs[0], frame);
+    return 0;
 }
 
-static const AVFilterPad avgclass_inputs[] = {
-    {
-        .name         = "default",
-        .type         = AVMEDIA_TYPE_VIDEO,
-        .filter_frame = filter_frame,
-    },
-};
+static int avgclass_activate(AVFilterContext *filter_ctx)
+{
+    AVFilterLink *inlink = filter_ctx->inputs[0];
+    AVFilterLink *outlink = filter_ctx->outputs[0];
+    AVFrame *in = NULL;
+    int ret, status;
+    int64_t pts;
 
-static const AVFilterPad avgclass_outputs[] = {
-    {
-        .name = "default",
-        .type = AVMEDIA_TYPE_VIDEO,
-    },
-};
+    FF_FILTER_FORWARD_STATUS_BACK(outlink, inlink);
+
+    ret = ff_inlink_consume_frame(inlink, &in);
+    if (ret < 0)
+        return ret;
+    if (ret > 0) {
+        ret = process_frame(filter_ctx, in);
+        if (ret < 0) {
+            av_frame_free(&in);
+            return ret;
+        }
+        ret = ff_filter_frame(outlink, in);
+        if (ret < 0)
+            return ret;
+    }
+
+    if (ff_inlink_acknowledge_status(inlink, &status, &pts)) {
+        if (status == AVERROR_EOF) {
+            ff_outlink_set_status(outlink, status, pts);
+            return 0;
+        }
+    }
+
+    FF_FILTER_FORWARD_WANTED(outlink, inlink);
+
+    return 0;
+}
 
 const FFFilter ff_vf_avgclass = {
     .p.name          = "avgclass",
@@ -151,6 +193,7 @@ const FFFilter ff_vf_avgclass = {
     .priv_size     = sizeof(AvgClassContext),
     .init          = init,
     .uninit        = uninit,
-    FILTER_INPUTS(avgclass_inputs),
-    FILTER_OUTPUTS(avgclass_outputs),
+    .activate      = avgclass_activate,
+    FILTER_INPUTS(ff_video_default_filterpad),
+    FILTER_OUTPUTS(ff_video_default_filterpad),
 };
